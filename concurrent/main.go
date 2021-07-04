ddd3bbc1eed1a2496d8e91630e7566e0707a481b
mark1002 <mark.chang@mile.cloud>
2021-07-04 16:07:47 +0800

    update concurrent pratice

--- concurrent/main.go
+++ concurrent/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func to_upper(in <-chan string) <-chan string {
+	out := make(chan string)
+	go func() {
+		for lang := range in {
+			upper_lang := strings.ToUpper(lang)
+			out <- upper_lang
+		}
+		close(out)
+	}()
+	return out
+}
+
+func producer(langs []string) <-chan string {
+	out := make(chan string)
+	go func() {
+		for _, lang := range langs {
+			out <- lang
+		}
+		close(out)
+	}()
+	return out
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	langs := []string{"python", "java", "ruby", "go", "c++"}
+	in := producer(langs)
+	out1 := to_upper(in)
+	out2 := to_upper(in)
+	for lang := range out2 {
+		fmt.Println("out2: " + lang)
+	}
+	for lang := range out1 {
+		fmt.Println("out1: " + lang)
+	}
+
+	fmt.Println("finish program.")
 }
