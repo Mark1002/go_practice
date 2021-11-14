532ba6817157982a0b07cdec860b0e50c182bce8
Mark Chang <ak0922747401@gmail.com>
2021-11-14 15:35:20 +0800

    refactor http.go and add goquery

--- concurrent/http.go
+++ concurrent/http.go
@@ -2,18 +2,31 @@ package concurrent
 
 import (
 	"fmt"
-	"io"
+	"log"
 	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 func ExecuteHttp() {
-	resp, err := http.Get("https://gobyexample.com/hello-world")
+	result := crawl("hello-world")
+	fmt.Println(result)
+}
+
+func crawl(path string) string {
+	resp, err := http.Get("https://gobyexample.com/" + path)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	fmt.Println("Response status:", resp.Status)
+	if resp.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", resp.StatusCode, resp.Status)
+	}
 	defer resp.Body.Close()
-	body, _ := io.ReadAll(resp.Body)
-	bodyString := string(body)
-	fmt.Println(bodyString)
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	title := doc.Find("h2").Text()
+	return title
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,9 @@ module github.com/mark1002/go_practice
 go 1.17
 
 require golang.org/x/tour v0.1.0
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+)
