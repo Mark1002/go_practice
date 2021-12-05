3000d3513959786b2d797222af0d4a22747f9f5e
Mark Chang <ak0922747401@gmail.com>
2021-12-05 15:45:56 +0800

    add switch statement in main.go

--- main.go
+++ main.go
@@ -1,18 +1,47 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/mark1002/go_practice/cmd"
+	"github.com/mark1002/go_practice/concurrent"
+	"github.com/mark1002/go_practice/error"
+	"github.com/mark1002/go_practice/fibclosure"
+	"github.com/mark1002/go_practice/hello"
+	"github.com/mark1002/go_practice/maps"
+	"github.com/mark1002/go_practice/slices"
 )
 
 func main() {
-	// error.ExecutePanic()
-	// error.ExecuteCustomError()
-	// concurrent.Execute()
-	// concurrent.ExecuteCrawler()
-	// concurrent.ExecuteHttp()
-	// slices.Execute()
-	// hello.Execute()
-	// fibclosure.Execute()
-	// maps.Execute()
-	cmd.Execute()
+	args := os.Args
+
+	if len(args) == 1 {
+		fmt.Println("please enter demo program name!")
+		os.Exit(0)
+	}
+	switch args[1] {
+	case "panicDemo":
+		error.ExecutePanic()
+	case "CustomErrorDemo":
+		error.ExecuteCustomError()
+	case "concurDemo":
+		concurrent.Execute()
+	case "crawlerDemo":
+		concurrent.ExecuteCrawler()
+	case "crawlerHttpDemo":
+		concurrent.ExecuteHttp()
+	case "slicesDemo":
+		slices.Execute()
+	case "helloDemo":
+		hello.Execute()
+	case "closureDemo":
+		fibclosure.Execute()
+	case "mapDemo":
+		maps.Execute()
+	case "cmdDemo":
+		cmd.Execute()
+	default:
+		fmt.Printf("not found program \"%v\"\n", args[1])
+	}
 }
